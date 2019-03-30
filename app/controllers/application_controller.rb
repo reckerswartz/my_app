890eb7bdc43f45ea7c9340e08a90e422d4c34f17
Recker Swartz <reckerswartz@hotmail.com>
2019-03-31 02:24:57 +0530

    protect_from_forgery updated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,6 @@
 
 class ApplicationController < ActionController::API
   include JSONAPI::ActsAsResourceController
-  protect_from_forgery with: :null_session
+
+  protect_from_forgery unless: -> { request.format.json? }
 end
