5a09c324cc3affbecb7e4484e7047bca122b0fb5
Recker Swartz <reckerswartz@hotmail.com>
2019-03-31 02:30:50 +0530

    ActionController::Flash updated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@
 class ApplicationController < ActionController::API
   include JSONAPI::ActsAsResourceController
   include ActionController::RequestForgeryProtection
+  include ActionController::Flash
 
 
   protect_from_forgery unless: -> { request.format.json? }
